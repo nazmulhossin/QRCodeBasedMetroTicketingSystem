caff58b1dadee14068324bcd626c713345c3cfa5
sujan-roy24 <sjnry25@gmail.com>
2025-01-05 23:14:20 +0600

    Day 1 Solves

---
+++ sujan-codes/Solution2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] inputs = Console.ReadLine().Split();
+        var nums = new List<int>();
+        for (int i = 0; i < 3; i++) {
+            int x = int.Parse(inputs[i]);
+            nums.Add(x);
+        }
+
+        nums.Sort();
+        bool ok = false;
+        if (nums[0] == nums[1] && nums[1] == nums[2]) {
+            ok = true;        
+        } else if (nums[0] + nums[1] == nums[2]) {
+            ok = true;
+        }
+        Console.WriteLine(ok ? "Yes" : "No");
+    }
+}
