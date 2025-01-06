9068d13efc9e92c4e67522697f4195d5257ee90b
sujan-roy24 <sjnry25@gmail.com>
2025-01-06 22:03:11 +0600

    Day 2 solutions

---
+++ sujan-codes/Solution4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main() 
+    {
+        string[] inputs = Console.ReadLine().Split();
+        int N = int.Parse(inputs[0]);
+        char c1 = inputs[1][0];
+        char c2 = inputs[2][0];
+        string S = Console.ReadLine();
+        char[] ans = S.ToCharArray();
+
+        for (int i = 0; i < ans.Length; i++) {
+            if (ans[i] != c1)
+                ans[i] = c2;
+        }
+        
+        Console.WriteLine(new string(ans));
+    }
+}
---
+++ sujan-codes/Solution5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main() 
+    {
+        string[] inputs = Console.ReadLine().Split(); 
+        int N = int.Parse(inputs[0]);
+        int Rating = int.Parse(inputs[1]);
+
+        for (int i = 0; i < N; i++) {
+            string[] s = Console.ReadLine().Split(); 
+            int D = int.Parse(s[0]);
+            int A = int.Parse(s[1]);
+            
+            if (D == 2 && Rating >= 1200 && Rating <= 2399) {
+                Rating += A;
+            }
+            else if (D == 1 && Rating >= 1600 && Rating <= 2799) {
+                Rating += A;
+            }
+        }
+        Console.WriteLine(Rating); 
+    }
+}
