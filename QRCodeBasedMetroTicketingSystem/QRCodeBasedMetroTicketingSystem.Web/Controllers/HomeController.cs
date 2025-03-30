c93f4b2ea52215e345ab6011a1b9c3a703bd0d3a
nazmul-hossin <mdnazmulhossin999@gmail.com>
2025-03-30 15:18:17 +0600

    Completed landing page design

--- QRCodeBasedMetroTicketingSystem/QRCodeBasedMetroTicketingSystem.Web/Controllers/HomeController.cs
+++ QRCodeBasedMetroTicketingSystem/QRCodeBasedMetroTicketingSystem.Web/Controllers/HomeController.cs
@@ -2,13 +2,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace QRCodeBasedMetroTicketingSystem.Web.Controllers;
 
+[Route("")]
 public class HomeController : Controller
 {
+    public IActionResult Root()
+    {
+        return RedirectToAction("Index", "Home");
+    }
+
+    [Route("Home")]
     public IActionResult Index()
     {
         return View();
     }
 
+    [Route("MapAndRoutes")]
     public IActionResult MapAndRoutes()
     {
         return View();
